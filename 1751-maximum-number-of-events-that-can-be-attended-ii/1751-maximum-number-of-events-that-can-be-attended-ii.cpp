a01dccdcbbef70214b0fd3e0def94c871ffb9173
Amanpreet Singh Setia <65528964+amanpreet9999@users.noreply.github.com>
2023-07-17 02:33:14 +0530

    Time: 317 ms (81.04%), Space: 75.8 MB (29.16%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>&dp, vector<vector<int>>& events, int n, int pos, int k)
-   {
-        int i;
+    int recurse(int idx, vector<vector<int>>& events, int k,vector<vector<int>>&dp){
         
-        if(pos >= n || k == 0)
+        if(k==0||idx>=events.size()){
             return 0;
-        
-        if(dp[pos][k] != -1)
-            return dp[pos][k];
-        
-        for(i = pos + 1; i < n; i++)
-            if(events[i][0] > events[pos][1])
+        }
+       if(dp[idx][k]!=-1)
+           return dp[idx][k];
+       int new_idx;
+        for(new_idx=idx+1;new_idx<events.size();new_idx++){
+            if(events[new_idx][0]>events[idx][1])
                 break;
+        }
         
-       return dp[pos][k] = max(solve(dp, events, n, pos+1, k), events[pos][2] + solve(dp, events, n, i, k-1));
-
+        int take = events[idx][2]+recurse(new_idx, events, k-1,dp);
+        int nonTake = recurse(idx+1,events,k,dp);
+        return dp[idx][k]=max(take,nonTake);
     }
-    
-    int maxValue(vector<vector<int>>& events, int k) 
-    {
-        int n = events.size();
-        sort(events.begin(), events.end());
-        
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        
-        return solve(dp, events, n, 0, k);
+    int maxValue(vector<vector<int>>& events, int k) {
+        vector<vector<int>> dp(events.size(),vector<int>(k+1,-1));
+        sort(events.begin(),events.end());
+        return recurse(0,events,k,dp);
     }
 };
