b5b6c7d25ce7f2f395514550233f44a6e2bf5f72
Amanpreet Singh Setia <65528964+amanpreet9999@users.noreply.github.com>
2023-07-28 00:25:52 +0530

    Time: 175 ms (80.22%), Space: 55.9 MB (19.42%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    
+    long long maxRunTime(int n, vector<int>& batteries) {
+        sort(batteries.rbegin(), batteries.rend());
+        long long sum=0;
+        for(int ele:batteries)
+            sum+=ele;
+        for(int i=0;i<batteries.size();i++,n--){
+            if(batteries[i]<=sum/n)
+                break;
+            else
+                sum-=batteries[i];
+            
+        }
+            return sum/n;
+    }
+};
