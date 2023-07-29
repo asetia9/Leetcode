c6fce7d1452bf7c7c15e755abc3c0ee668c3fbb1
Amanpreet Singh Setia <65528964+amanpreet9999@users.noreply.github.com>
2023-07-30 00:20:18 +0530

    Time: 12 ms (49.05%), Space: 138.4 MB (24.33%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double helper(int a, int b, vector<vector<double>> &dp){
+        if(a<=0&&b<=0){
+            return 0.5;
+        }
+        if(a<=0){
+            return 1.0;
+        }
+        if(b<=0){
+            return 0.0;
+        }
+        if(dp[a][b]!=-1.0)
+            return dp[a][b];
+        dp[a][b]=0.0;
+        dp[a][b]+=helper(a-100,b,dp);
+        dp[a][b]+=helper(a-75,b-25,dp);
+        dp[a][b]+=helper(a-50,b-50,dp);
+        dp[a][b]+=helper(a-25,b-75,dp);
+        dp[a][b]*=0.25;
+        return dp[a][b];
+    }
+    
+    
+    double soupServings(int n) {
+        if(n>5000)
+            return 1;
+        vector<vector<double>> dp(n+1,vector<double>(n+1,-1.0));
+        return helper(n,n,dp);    
+    }
+};
