90cd5a67895ba3694bfdf200b8574de9cdbac3df
Amanpreet Singh Setia <65528964+asetia9@users.noreply.github.com>
2023-08-06 02:54:38 +0530

    Time: 11 ms (95.90%), Space: 15.6 MB (70.76%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode *> recurse(int low, int high){
+        if(low==high){
+            TreeNode * nh = new TreeNode(low);
+            return {nh};
+        }
+        if(low>high){
+            return {NULL};
+        }
+        vector<TreeNode *> ans;
+        for(int i=low;i<=high;i++){
+            vector<TreeNode *> left = recurse(low,i-1);
+            vector<TreeNode *> right = recurse(i+1,high);
+            if(left.size()&&right.size()){
+                for(TreeNode * r:right){
+                    for(TreeNode *l:left){
+                        
+                        TreeNode *root = new TreeNode(i);
+                        root->left = l;
+                        root->right = r;
+                        ans.push_back(root);
+                    }
+                }
+            }
+            else if(left.size()){
+                for(TreeNode *l:left){
+                    TreeNode *root = new TreeNode(i);
+                    root->left=l;
+                    ans.push_back(root);
+                }
+                
+            }
+            else if(right.size()){
+                for(TreeNode *r:right){
+                    TreeNode *root = new TreeNode(i);
+                    root->left=r;
+                    ans.push_back(root);
+                }
+                
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+    vector<TreeNode*> generateTrees(int n) {
+        return recurse(1,n);
+    }
+};
